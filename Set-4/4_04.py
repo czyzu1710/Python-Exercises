b0de3858db3495c3f4efa0eea61c7e901a3eb374
Michał Czyż <czyzu1710@gmail.com>
2019-10-30 21:56:34 +0100

    Repaired 4_04. Turns out it was computating Fibonacci numbers but not given.

--- Set-4/4_04.py
+++ Set-4/4_04.py
@@ -3,11 +3,11 @@
 
 def fibonacci(n):
     result = [0,1]
-    for x in range(2,n):
+    for x in range(2,int(n/2) + 2):
         result[0] = result[0] + result[1]
         result[1] = result[0] + result[1]
-    
-    return result[1] 
+
+    return result[n%2]
 
 
-print(fibonacci(4))
+print(fibonacci(19))
