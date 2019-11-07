6447a5c8744f5411651e01c73301600f8afb4a4e
Michał Czyż <czyzu1710@gmail.com>
2019-11-07 20:11:25 +0100

    Completed test file. Fixed fracs.py. Stable version.

--- Set-5/5_02.py
+++ Set-5/5_02.py
@@ -17,8 +17,40 @@ class TestFractions(unittest.TestCase):
     
 
     def test_add_frac(self):
-        self.assertEquals(add_frac( self.half, self.oneThird),[5,6])
+        self.assertEqual(add_frac(self.half, self.oneThird),[5,6])
+        self.assertEqual(add_frac(self.half, self.half), [1,1])
+        self.assertEqual(add_frac(self.half, self.oneQuarter),[3,4])
+        self.assertEqual(add_frac(self.oneQuarter, self.oneThird), [7,12])
+    
+    
+    def test_sub_frac(self):
+        self.assertEqual(sub_frac(self.half, self.oneQuarter),[1,4])
+        self.assertEqual(sub_frac([3,4],[1,4]),[1,2])
+        self.assertEqual(sub_frac(self.half, self.minOneQuarter),[3,4])
+    
+    
+    def test_mul_frac(self):
+        self.assertEqual(mul_frac(self.half,self.half),[1,4])
+        self.assertEqual(mul_frac(self.half,self.oneQuarter), [1,8])
+        self.assertEqual(mul_frac(self.oneThird, self.half),[1,6])
+   
 
+    def test_div_frac(self):
+        self.assertEqual(div_frac(self.half,self.half),[1,1])
+        self.assertEqual(div_frac(self.half,self.oneQuarter), [2,1])
+        self.assertEqual(div_frac(self.oneThird, self.half),[2,3])
+    
 
+    def test_isPositive(self):
+        self.assertTrue(isPositive(self.half))
+        self.assertFalse(isPositive(self.minhalf))
+
+    def test_isZero(self):
+        self.assertTrue(isZero(self.zero))
+        self.assertFalse(isZero(self.oneQuarter))
+    def test_cmp_frac(self):
+        self.assertEqual(cmp_frac(self.half, [1,2]),0)
+        self.assertEqual(cmp_frac(self.half, self.threeQuarters),-1)
+        self.assertEqual(cmp_frac([3,4], self.half),1)
 if __name__ == '__main__':
     unittest.main()
--- Set-5/fracs.py
+++ Set-5/fracs.py
@@ -1,6 +1,7 @@
 #!usr/bin/python
 #-*- coding: utf-8 -*-
-import fractions
+
+from math import gcd
 
 def add_frac(frac_1, frac_2):
     result = [0,0] 
@@ -9,10 +10,15 @@ def add_frac(frac_1, frac_2):
         frac_2[0] *= frac_1[1]
         frac_1[1]  = frac_2[1] = frac_1[1] * frac_2[1]
         
-        result[0] = frac_1[0] + frac_2[0]
-        result[1] = frac_1[1]
-        gcd  = fractions.gcd(result[0], result[1])
-        return [result[0]/gcd, result[1]/gcd]
+    result[0] = frac_1[0] + frac_2[0]
+    result[1] = frac_1[1]
+    if result[0] < 0:
+        result[0], result[1] = result[0]*(-1),result[1]*(-1) 
+    if result[0] == result[1]:
+         gcd1 = result[0]
+    else:
+        gcd1  = gcd(result[0], result[1])
+    return [result[0]/gcd1, result[1]/gcd1]
 
 
 def sub_frac(frac_1, frac_2):
@@ -24,15 +30,24 @@ def mul_frac(frac_1, frac_2):
     result = [0,0]
     result[0] = frac_1[0] * frac_2[0]
     result[1] = frac_1[1] * frac_2[1]
-    gcd = fractions.gcd(result[0],result[1])
-    return [result[0]/gcd, result[1]/gcd]
+
+    if result[0] < 0 :
+        result[0]*= -1
+        result[1]*= -1
+    if result[0] == result[1]:
+        gcd1 = result[0]
+    else:
+        gcd1 = gcd(result[0],result[1])
+    return [result[0]/gcd1, result[1]/gcd1]
 
 def div_frac(frac_1, frac_2):
-    return mul_frac(frac_1, frac_2.reverse())
+    frac = list(frac_2)
+    frac.reverse()
+    return mul_frac(frac_1,frac)
 
 
 def isPositive(frac_1):
-    return frac_[0] > 0 and frac_1[1] > 0
+    return frac_1[0] > 0 and frac_1[1] > 0
 
 
 def isZero(frac_1):
@@ -42,8 +57,10 @@ def cmp_frac(frac_1, frac_2):
     frac = sub_frac(frac_1, frac_2)
     if frac[0] == 0:
         return 0
-    elif (frac[0] < 0 and frac[1] > 0) or (frac[0] > 0 and frac[1] < 0):
+
+    if (frac[0] < 0 and frac[1] > 0) or (frac[0] > 0 and frac[1] < 0):
         return -1
+
     else:
         return 1
 
